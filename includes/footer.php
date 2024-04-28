11f1e4fc4cdfda7cdfbdb41ed7adf483aa280695
Dan Lloyd "Ayato" Cabanilla <139518282+DLAyatoCabanilla@users.noreply.github.com>
2024-04-29 04:13:55 +0800

    Part of Index.php

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2024 Brought to You By <a href="https://github.com/DLAyatoCabanilla/E-Commerce-PHP">Lumiliyab Na Apoy</a></strong>
+    </div>
+</footer>
